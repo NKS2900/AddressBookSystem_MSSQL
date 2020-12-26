72aec9f90c7b5f5cccfdb214ae77a329e0ddc330
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-26 18:30:48 +0530

    [NIJAM] Add:Insert new Records in addressBook.

---
+++ InsertDataInTable.sql
@@ -0,0 +1,6 @@
+insert into dbo.address_book values
+('Nijam', 'Sayyad', 'Shivaji_Choak', 'Latur', 'Maharashtra', 431517, '9130918545', 'nks@gmail.com'),
+('Imran', 'Shaikh', 'Dighi', 'Pune', 'Maharashtra', 411014, '9245632154', 'imsh@gmail.com'),
+('Dipak', 'Nagargoje', 'nagar', 'pune', 'Maharashtra', 431347, '9112365478', 'dpk@gmail.com'),
+('Mahesh', 'Kande', 'parli', 'Latur', 'Maharashtra', 431434, '9987456120', 'mkas@gmail.com')
+

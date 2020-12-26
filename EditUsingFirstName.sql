04c95cbc877aea5b6bffa7ae3ba0f28dbbc6077c
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-26 18:43:25 +0530

    [NIJAM] Add:Edit Existing Contact Using First_Name.

---
+++ EditUsingFirstName.sql
@@ -0,0 +1,8 @@
+update [dbo].[address_book] set city = 'Beed' 
+where first_name = 'Nijam';
+
+update [dbo].[address_book] set address = 'nagadara' 
+where first_name = 'Dipak';
+
+update [dbo].[address_book] set city = 'Californiya' 
+where first_name = 'Imran';

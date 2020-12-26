4c7b19e5d100ea69f805782884450ed10ba116b0
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-26 19:25:41 +0530

    [NIJAM] Add:Sort Contacts by First Name Using City.

---
+++ SortedContactsByName.sql
@@ -0,0 +1,8 @@
+select * from [dbo].[address_book]
+where city = 'Latur' order by first_name asc;
+
+select first_name, last_name from [dbo].[address_book]
+where city = 'Pune' order by first_name asc;
+
+select * from [dbo].[address_book]
+where city = 'Beed' order by first_name asc;

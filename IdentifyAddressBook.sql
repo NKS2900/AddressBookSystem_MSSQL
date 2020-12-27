92a932c6cc160d1a9f96cce33a093ac7911d8980
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-27 10:24:51 +0530

    [NIJAM] Add:alter table and add AddressBookName and Type.

--- IdentifyAddressBook.sql
+++ IdentifyAddressBook.sql
@@ -1,13 +1,17 @@
-alter table [dbo].[address_book] add bookname varchar(20)
+alter table [dbo].[address_book] add bookname varchar(20) not null default 'address001'
+alter table [dbo].[address_book] add addressbooktype varchar(20) not null default 'family'
 
-update [dbo].[address_book] set bookname= 'family' where first_name = 'Nijam'
-update [dbo].[address_book] set bookname = 'family' where first_name = 'Imran'
-update [dbo].[address_book] set bookname= 'friend' where first_name = 'Dipak'
-update [dbo].[address_book] set bookname = 'family' where first_name = 'Mohin'
-update [dbo].[address_book] set bookname = 'office' where first_name = 'Irshad'
-update [dbo].[address_book] set bookname= 'friend' where first_name = 'Vishal'
-update [dbo].[address_book] set bookname = 'office' where first_name = 'Mahesh'
 
-select * from dbo.address_book where bookname='family'
-select * from dbo.address_book where bookname='friend'
-select first_name,last_name,phone_number from [dbo].[address_book] where bookname='office'
+update [dbo].[address_book] set bookname= 'address001' where first_name = 'Nijam'
+update [dbo].[address_book] set bookname = 'address001' where first_name = 'Imran'
+update [dbo].[address_book] set bookname= 'address002' where first_name = 'Dipak'
+update [dbo].[address_book] set bookname = 'address001' where first_name = 'Mohin'
+update [dbo].[address_book] set bookname = 'address003' where first_name = 'Irshad'
+update [dbo].[address_book] set bookname= 'address002' where first_name = 'Vishal'
+update [dbo].[address_book] set bookname = 'address003' where first_name = 'Mahesh'
+
+update [dbo].[address_book] set addressbooktype = 'family' where bookname='address001'
+update [dbo].[address_book] set addressbooktype = 'friend' where bookname='address002'
+update [dbo].[address_book] set addressbooktype = 'office' where bookname='address003'
+
+select * from [dbo].[address_book] where addressbooktype='family'

a83297cd3f504bf155119795874e6e77d6457210
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-27 18:19:33 +0530

    [NIJAM] Add:Insert record in both type friend family or office.

---
+++ InsertRecordByType.sql
@@ -0,0 +1,7 @@
+insert into [dbo].[address_book] values
+('Ganesh', 'jadhav', 'pusad', 'Beed', 'Maharashtra', 434343, '9638527410', 'ganeshj@gmail.com', 'address002', 'friend'),
+('Vivek', 'jadhav', 'Fstreet', 'Dhule', 'Gao', 543234, '7418529630', 'vkj@gmail.com', 'address003', 'office'),
+('vikas', 'kamble', 'kaij', 'Parbhani', 'Keral', 434545, '8794561230', 'vikas@gmail.com', 'address003', 'office')
+
+
+select * from dbo.address_book
